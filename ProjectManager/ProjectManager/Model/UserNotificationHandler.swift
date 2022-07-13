3dcf2fdf6dd765812f121e52830af704931a8ae1
yeahg-dev <yeahg.dev@gmail.com>
2022-07-13 15:59:53 +0900

    feat: UserNotificationHandler 구현 #10

---
+++ ProjectManager/ProjectManager/Model/UserNotificationHandler.swift
@@ -0,0 +1,44 @@
+//
+//  UserNotificationHandler.swift
+//  ProjectManager
+//
+//  Created by 1 on 2022/07/13.
+//
+
+import Foundation
+import UserNotifications
+
+struct UserNotificationHandler {
+    
+    private let userNotificationCenter = UNUserNotificationCenter.current()
+    
+    func requestNotification(of content: UserNotificationContent,
+                             when dateComponent: DateComponents,
+                             identifier: String) {
+        let content = UNMutableNotificationContent()
+        content.title = content.title
+        content.body = content.body
+        
+        let trigger = UNCalendarNotificationTrigger(
+                 dateMatching: dateComponent, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: identifier,
+                    content: content, trigger: trigger)
+
+        userNotificationCenter.add(request) { (error) in
+           if error != nil {
+              // Handle any errors.
+           }
+        }
+    }
+    
+    func removeNotification(of identifier: String) {
+        userNotificationCenter.removePendingNotificationRequests(withIdentifiers: [identifier])
+    }
+}
+
+struct UserNotificationContent {
+    
+    let title: String?
+    let body: String?
+}
